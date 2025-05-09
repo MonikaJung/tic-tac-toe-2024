b83bbf21f0a043185e061f88defab5937838367a
Monika Jung <MonikaJung@users.noreply.github.com>
2025-05-09 23:20:20 +0200

    corrected frontend fetch with env variable of backend IP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ function App() {
           onBackendError={handleBackendError}
           usernameX={usernameX}
           usernameO={usernameO}
-          backendAddress={"backend"} // on EC2 "backend", locally "localhost"
+          backendAddress={process.env.REACT_APP_BACKEND_URL} // on EC2 IPv4 of backend, locally "localhost"
         />
       )}
     </div>
