f0bbc7d7a276f34c59bde873bc3ae2fb05339010
Monika Jung <MonikaJung@users.noreply.github.com>
2025-05-20 19:09:30 +0200

    Changed to backend public IP

--- terraform/main-two-ec2.tf
+++ terraform/main-two-ec2.tf
@@ -101,7 +101,7 @@ resource "aws_security_group" "backend_sg" {
     from_port       = 8080  # Backend service
     to_port         = 8080
     protocol        = "tcp"
-    security_groups = [aws_security_group.frontend_sg.id]  # Only from frontend
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -140,7 +140,7 @@ resource "aws_instance" "frontend_instance" {
   key_name                    = aws_key_pair.tic_tac_toe_key.key_name
 
   user_data = templatefile("frontend-userdata.sh.tpl", {
-    backend_ip = aws_instance.backend_instance.private_ip
+    backend_ip = aws_instance.backend_instance.public_ip
   })
 
   tags = {
@@ -154,10 +154,6 @@ output "backend_public_ip" {
 
 output "frontend_public_ip" {
   value = aws_instance.frontend_instance.public_ip
-}
-
-output "backend_private_ip" {
-  value = aws_instance.backend_instance.private_ip
 }
 
 output "frontend_url" {
